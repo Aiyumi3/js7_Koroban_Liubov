f6815823c733409ad54382661c37da0e6cf569ea
Aiyumi Sunshine <62023811+Aiyumi3@users.noreply.github.com>
2020-11-04 23:15:44 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,23 +1,18 @@
-/* Индекс слайда по умолчанию */
 let slideIndex = 1;
 showSlides(slideIndex);
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
     showSlides(slideIndex += 1);
 }
 
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
 function minusSlide() {
     showSlides(slideIndex -= 1);
 }
 
-/* Устанавливает текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-/* Основная функция слайдера */
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName('item');
@@ -31,5 +26,4 @@ function showSlides(n) {
         slides[i].style.display = 'none';
     }
     slides[slideIndex - 1].style.display = 'block';
-
 }
